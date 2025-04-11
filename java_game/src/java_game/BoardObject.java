45bfb6c54ed917345232bc8783f92cec2bc6cc15
cpcervan <cpcervan@ncsu.edu>
2025-04-10 21:52:21 -0400

    Changed images to scale with tile size

--- java_game/src/java_game/BoardObject.java
+++ java_game/src/java_game/BoardObject.java
@@ -1,7 +1,9 @@
 package java_game;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.RenderingHints;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
@@ -24,10 +26,26 @@ public abstract class BoardObject {
 	        // you can use just the filename if the image file is in your
 	        // project folder, otherwise you need to provide the file path.
 	        image = ImageIO.read(new File(path));
+	        int newWidth = Board.TILE_SIZE;
+            int newHeight = Board.TILE_SIZE;
+
+            // Scale the image
+            image = scaleImage(image, newWidth, newHeight);
 	    } catch (IOException exc) {
 	        System.out.println("Error opening image file: " + exc.getMessage());
 	    }
 	}
+	
+	private static BufferedImage scaleImage(BufferedImage originalImage, int newWidth, int newHeight) {
+        BufferedImage scaledImage = new BufferedImage(newWidth, newHeight, originalImage.getType());
+        Graphics2D g2d = scaledImage.createGraphics();
+
+        // Improve scaling quality
+        g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g2d.drawImage(originalImage, 0, 0, newWidth, newHeight, null);
+        g2d.dispose();
+        return scaledImage;
+    }
 
 	public void draw(Graphics g, ImageObserver observer) {
 	    // with the Point class, note that pos.getX() returns a double, but 
